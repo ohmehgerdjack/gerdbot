ebb1856e9786b3492ce972c154a24e7c53c1f1a0
ohmehgerdjack <ohmehgerdjack@gmail.com>
2015-05-21 22:33:04 +0100

    Update extension.js

--- extension.js
+++ extension.js
@@ -114,6 +114,6 @@
     }));
 
     //Start the bot and extend it when it has loaded.
-    $.getScript("https://rawgit.com/Yemasthui/basicBot/master/basicBot.js", extend);
+    $.getScript("https://rawgit.com/ohmehgerdjack/gerdbot/master/Code", extend);
 
 }).call(this);
